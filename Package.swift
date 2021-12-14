a5d133dd4ad37bcd55496d55ac3679060356d17e
Tibor Bödecs <mail.tib@gmail.com>
2021-12-14 14:34:30 +0100

    dep updates

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "liquid",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v12)
     ],
     products: [
         .library(name: "Liquid", targets: ["Liquid"]),
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.50.0"),
-        .package(url: "https://github.com/binarybirds/liquid-kit.git", from: "1.3.0"),
+        .package(url: "https://github.com/binarybirds/liquid-kit.git", from: "1.3.2"),
     ],
     targets: [
         .target(name: "Liquid", dependencies: [
